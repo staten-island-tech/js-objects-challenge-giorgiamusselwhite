2c0be599b1a226ec5fc3c44fccb6575276c0fdeb
Gia Musselwhite <56314402+giorgiamusselwhite@users.noreply.github.com>
2020-04-22 06:55:57 -0400

    notes

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const Angel = {};
 Angel.dob = Riya.dob; */
 
 
-const presidents = [
+/* const presidents = [
     { name: "Donald Trump", terms: 1, party: "R" },
     { name: "Barack Obama", terms: 2, party: "D" },
     { name: "George W. Bush", terms: 2, party: "R" },
@@ -74,7 +74,7 @@ const presidents = [
     { name: "John F. Kennedy", terms: 1, party: "D" },
     { name: "Lyndon Johnson", terms: 2, party: "D" },
     { name: "Dwight Eisenhower", terms: 2, party: "R" }
-]
+] */
 
 /* //challenge q1
 
@@ -113,3 +113,81 @@ console.log (2_term_D); */
 }
 };
  */
+
+
+ //const header = document.querySelector(".header");
+/* const items = document.querySelectorAll(".list-item");
+
+const itemsArr = Array.from(items);
+
+itemsArr.forEach((element) => (element.style.backgroundColor = "red")); */
+
+//const header = document.querySelector("#Idheader");
+
+/* const header = document.getElementById("Idheader");
+
+const toggle = document.querySelector(".toggle"); */
+
+//header.classList.add("Karen");
+
+//header.classList.remove("Karen");
+
+/* toggle.addEventListener("click", function () {
+
+header.classList.toggle("Karen");
+
+});
+
+*/
+
+//button click change background to red
+
+//button click change background to blue
+
+
+const background = document.querySelector(".background");
+
+const button = document.querySelector(".button");
+
+const headerTwo = document.querySelector(".header-two");
+
+
+
+/* changeBtn.addEventListener("click", function () {
+
+if (background.style.backgroundColor !== "rgb(255, 0, 0)") {
+
+background.style.backgroundColor = "rgb(255, 0, 0)";
+
+headerTwo.textContent = "Red";
+
+} else {
+
+background.style.backgroundColor = "rgb(0, 0, 255)";
+
+headerTwo.textContent = "Blue";
+
+}
+
+});
+
+*/
+
+///3 numbers for R G and B
+
+//get random number, make random number usable
+
+
+button.addEventListener("click", function () {
+
+const R = Math.floor(Math.random() * 255); //scoped variable
+
+const G = Math.floor(Math.random() * 255);
+
+const B = Math.floor(Math.random() * 255);
+
+background.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
+
+headerTwo.textContent = `rgb(${R}, ${G}, ${B})`;
+
+});
